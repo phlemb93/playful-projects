4a52f897623a483b073a015f49970a0fe7fda219
Badrudeen Oluwafemi <phlemb6362@gmail.com>
2022-07-24 16:12:57 +0100

    creating a css framework using sass

---
+++ sass/gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+
+
+
+function buildStyles() {
+    return src('index.scss')
+    .pipe(sass())
+    .pipe(dest('css'))
+}
+
+function watchBuild() {
+    watch(['/**/*.scss'], buildStyles);
+}
+
+exports.default = series(buildStyles, watchBuild);
